2c3710cc7d29068b2bb5a3c7480c7f9a4c3a1dd3
constantine <glowforever42@gmail.com>
2023-07-17 11:44:50 +0900

    test

---
+++ src/app/welcome/hell/page.js
@@ -0,0 +1,8 @@
+export default function hell() {
+  return (
+    <div>
+      <h4 className='title '>지옥</h4>
+      <p className='title-sub'>by 휘</p>
+    </div>
+  )
+}
---
+++ src/app/welcome/page.js
@@ -0,0 +1,8 @@
+export default function welcome() {
+  return (
+    <div>
+      <h4 className='title '>반가워용</h4>
+      <p className='title-sub'>by 휘</p>
+    </div>
+  )
+}
