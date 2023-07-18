7544ef7ba172c5f88feef1b4b57bb75bddfa4d64
constantine <glowforever42@gmail.com>
2023-07-18 23:46:00 +0900

    블릿 마크업 테스트

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,18 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h4 className='title '>하이</h4>
-      <p className='title-sub'>by 휘</p>
+      <div className='title-box'>
+        <h3 className='title'>JB 모임</h3>
+        <div className='sub-title-box'>
+          <h5 className='sub-title'>JB 친구 만들기</h5>
+          <h4 className='sub-content'>JB 모임을 시작해보세요</h4>
+        </div>
+      </div>
+      <div>
+        <div className='welcome-box'>
+          <h1>지금 만나요 😝</h1>
+        </div>
+      </div>
     </div>
   )
 }
